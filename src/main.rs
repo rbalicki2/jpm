827e2dad26380019799601a99aa072428825f302
Robert Balicki <robertbalicki@fb.com>
2022-06-15 09:14:47 -0400

    v1: demonstrate enum

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,30 @@
+use std::path::PathBuf;
 use structopt::StructOpt;
 
-#[derive(Debug, StructOpt)]
-#[structopt(name = "example", about = "An example!")]
-
-struct Opt {
-    #[structopt(short, long)]
-    foo: bool,
+#[derive(StructOpt, Debug)]
+#[structopt(about = "the stupid content tracker")]
+enum Opt {
+    Add {
+        #[structopt(short)]
+        interactive: bool,
+        #[structopt(short)]
+        patch: bool,
+        #[structopt(parse(from_os_str), long, short)]
+        files: Vec<PathBuf>,
+    },
+    Fetch {
+        #[structopt(long)]
+        dry_run: bool,
+        #[structopt(long)]
+        all: bool,
+        repository: Option<String>,
+    },
+    Commit {
+        #[structopt(short)]
+        message: Option<String>,
+        #[structopt(short)]
+        all: bool,
+    },
 }
 
 fn main() {
